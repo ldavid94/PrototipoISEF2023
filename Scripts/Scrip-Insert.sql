d12e2191551904b74dfb9553d949f4ef333f0120
ldavid94 <ldavid92394@gmail.com>
2023-06-15 11:34:27 -0600

    Se esta subiendo el scrip y los insert necesarios

---
+++ Scripts/Scrip-Insert.sql
@@ -0,0 +1,21 @@
+/*--Insert pra la tabla tbl_tipoexamen--*/
+
+insert into tbl_tipoexamen values (1,'Hemograma completo','analisis de sangre');
+insert into tbl_tipoexamen values (2,'Perfil de lipidos','mide las concentraciones de grasa');
+insert into tbl_tipoexamen values (3,'Quimica sanguinia','mide los nivels electrolitos');
+insert into tbl_tipoexamen values (4,'Perfil tiroide','evaluacion de la glandula tiroideo');
+insert into tbl_tipoexamen values (5,'Prueba ELISA','Examen para detectar anticuerpos');
+
+/*--Insert pra la tabla tbl_tipoexamen--*/
+
+insert into tbl_empleados values (1,'Luis','Reyes','55234656','22364648','zona 6','1994-09-23',1);
+insert into tbl_empleados values (2,'Daniel','Paz','55234656','22364648','zona 16','1996-07-11',2);
+insert into tbl_empleados values (3,'Maricel','Ortiz','55234656','22364648','zona 7','1966-03-03',3);
+insert into tbl_empleados values (4,'Gerson','Santos','55234656','22364648','zona 8','1972-09-11',4);
+insert into tbl_empleados values (5,'Carlos','Marroquin','55234656','22364648','zona 10','1995-05-12',5);
+
+/*--Insert pra la tabla tbl_pago--*/
+insert into tbl_pago values (1,'Enfectivo');
+insert into tbl_pago values (2,'Tarjeta de Debito/credito');
+insert into tbl_pago values (3,'Deposito');
+
